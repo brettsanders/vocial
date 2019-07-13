db4c6a825526437b3a0472a4dafabb73ac5a5774
Brett Sanders <brettrsanders@gmail.com>
2019-07-13 13:38:32 -0400

    Voting on Option per socket

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,6 +1,26 @@
 // Import Phoenix's Socket Library
 import { Socket } from 'phoenix';
 
+// Utility Functions
+const pushVote = (el, channel) => {
+  channel
+    .push('vote', {option_id: el.getAttribute('data-option-id')})
+    .receive('ok', res => console.log('You voted!'))
+    .receive('error', res => console.log('Failed to vote:', res));
+};
+
+const onJoin = (res, channel) => {
+  document.querySelectorAll('.vote-button-manual').forEach(el => {
+    el.addEventListener('click', event => {
+      event.preventDefault();
+      pushVote(el, channel);
+    });
+  });
+
+  console.log('Joined channel:', res);
+};
+
+
 // Next, create a new Phoenix Socket to reuse
 const socket = new Socket('/socket');
 
@@ -12,11 +32,15 @@ const channel = socket.channel('polls:lobby', {});
 if (document.getElementById('enable-polls-channel')) {
   channel
     .join()
-    .receive('ok', res => console.log('Joined channel:', res))
+    .receive('ok', res => onJoin(res, channel))
     .receive('error', res => console.log('Failed to join channel:', res));
 
   channel.on('pong', payload => {
     console.log("The server has been PONG'd and all is well:", payload);
+  });
+
+  channel.on('new_vote', ({ option_id, votes }) => {
+    document.getElementById('vote-count-' + option_id).innerHTML = votes;
   });
 
   document.getElementById("polls-ping").addEventListener("click", () => {
