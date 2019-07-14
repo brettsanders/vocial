0a21fb19edc390e94d142470facf0e8c6fab71c1
Brett Sanders <brettrsanders@gmail.com>
2019-07-14 18:21:53 -0400

    Add Images migration, form fields

--- lib/vocial_web/endpoint.ex
+++ lib/vocial_web/endpoint.ex
@@ -14,6 +14,8 @@ defmodule VocialWeb.Endpoint do
     only: ~w(css fonts images js favicon.ico robots.txt)
   )
 
+  plug(Plug.Static, at: "/uploads", from: "./uploads")
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
